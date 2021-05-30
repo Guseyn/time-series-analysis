a01b220b850c22977b6f2efeed6f5adf8b926cfa
guseyn <guseynism@gmail.com>
2021-05-30 14:30:09 +0200

    charts with cointegrated sequnces

---
+++ main.R
@@ -0,0 +1,20 @@
+data <- read.csv('data.csv', header = TRUE, stringsAsFactors = FALSE)
+xLength <- length(data$date)
+xPointsForDates <- 1:xLength
+
+for (i in 1:10) {
+  for (j in (i + 1):10) {
+    pdfName <- sprintf('rplot_y%d_and_y%d.pdf', i, j)
+    # Open a pdf file
+    pdf(pdfName)
+    # Create a plot
+    plot(x = xPointsForDates, y = data[[sprintf('y%d', i)]],
+         pch = 16, frame = FALSE,
+         xlab = 'dates', ylab = c('y', i), col = '#2E9FDF')
+    points(x = xPointsForDates, y = data[[sprintf('y%d', j)]], col = '#Df2E7E', pch = 19)
+    # Close the pdf file
+    dev.off() 
+    # Print that PDF is generated
+    print(sprintf('PDF %s with charts for y%d and y%d is generated', pdfName, i, j))
+  }
+}
