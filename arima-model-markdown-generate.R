fa54b5616fc23f3d2bc445036c7c2ff4ffbe090b
guseyn <guseynism@gmail.com>
2021-07-05 18:41:15 +0200

    charts for var model

--- arima-model-markdown-generate.R
+++ arima-model-markdown-generate.R
@@ -1,5 +1,8 @@
 ## install.packages("rmarkdown", repos = "http://cran.us.r-project.org")
 ## install.packages("tinytex", repos = "http://cran.us.r-project.org")
 ## tinytex::install_tinytex()
+
+# in terminal: export RSTUDIO_PANDOC=/Applications/RStudio.app/Contents/MacOS/pandoc
+
 library(rmarkdown)
 rmarkdown::render("arima-markdown.Rmd", clean=TRUE, output_format="pdf_document")
